fca262efc6a00fb9048fa901a0d62029e4415ad3
Coşkun DENİZ <coskundeniz1989@gmail.com>
2017-02-09 15:47:20 +0300

    Removed thymeleaf because unnecessary

--- spring-boot-work-hikaricp/src/main/java/my/gradle/SpringBootExampleApplication.java
+++ spring-boot-work-hikaricp/src/main/java/my/gradle/SpringBootExampleApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @SpringBootApplication
 public class SpringBootExampleApplication {
