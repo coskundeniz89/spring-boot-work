9e7463eb9d208a8cba82a525102ec138e6a004b0
Coşkun DENİZ <coskundeniz1989@gmail.com>
2017-02-13 15:36:54 +0300

    Added spring core project.

---
+++ spring-boot-work-core/README.md
@@ -0,0 +1,3 @@
+## Spring Boot 
+#### Run Application
+./gradlew build && java -jar build/libs/spring-boot-work-core.jar
---
+++ spring-boot-work-core/src/test/java/tr/cd/ApplicationTest.java
@@ -0,0 +1,25 @@
+package tr.cd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by coskun.deniz on 06.02.2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+}
