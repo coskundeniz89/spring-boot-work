2c75e6c6556e4ec8e654c764df3dc882ec4305ca
Erhan Karakaya <erhan277@gmail.com>
2017-02-20 14:55:40 +0300

    Added a list example

--- spring-boot-work-thymeleaf/src/main/java/tr/cd/HelloController.java
+++ spring-boot-work-thymeleaf/src/main/java/tr/cd/HelloController.java
@@ -1,7 +1,11 @@
 package tr.cd;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by coskun.deniz on 09.02.2017.
@@ -12,6 +16,21 @@ public class HelloController {
   @RequestMapping("/")
   public String getIndex() {
     return "index";
+  }
+
+  @RequestMapping("/list")
+  public String getList(Model model) {
+
+    List<Product> products = new ArrayList<>();
+    products.add(new Product(1,"Kalem",10));
+    products.add(new Product(2,"Silgi",20));
+    products.add(new Product(3,"Defter",5));
+    products.add(new Product(4,"Kitap",6));
+    products.add(new Product(5,"Dosya",16));
+
+    model.addAttribute("prods",products);
+
+    return "list";
   }
 
 }
