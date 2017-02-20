f46a3190e639189e7aaef06b97523cc7c1b8ea34
Erhan Karakaya <erhan277@gmail.com>
2017-02-20 14:52:27 +0300

    An example Product class is added

---
+++ spring-boot-work-thymeleaf/src/main/java/tr/cd/Product.java
@@ -0,0 +1,41 @@
+package tr.cd;
+
+/**
+ * Created by erhan.karakaya on 2/20/2017.
+ */
+public class Product {
+
+  public Product(Integer id, String title, Integer quantity) {
+    this.id = id;
+    this.title = title;
+    this.quantity = quantity;
+  }
+
+  private Integer id;
+  private String title;
+  private Integer quantity;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+}
