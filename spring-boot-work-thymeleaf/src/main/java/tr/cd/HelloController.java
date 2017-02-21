984b3258b9ee6ed08514703aca3edcbac8cf6528
Erhan Karakaya <erhan277@gmail.com>
2017-02-21 10:37:24 +0300

    Added detail example

--- spring-boot-work-thymeleaf/src/main/java/tr/cd/HelloController.java
+++ spring-boot-work-thymeleaf/src/main/java/tr/cd/HelloController.java
@@ -3,6 +3,8 @@ package tr.cd;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +20,11 @@ public class HelloController {
     return "index";
   }
 
+  /**
+   * list of products.
+   * @param model
+   * @return product list
+   */
   @RequestMapping("/list")
   public String getList(Model model) {
 
@@ -27,5 +34,23 @@ public class HelloController {
 
     return "list";
   }
+
+
+  /**
+   * detail of a product.
+   * @param id
+   * @param model
+   * @return product
+   */
+  @RequestMapping(value = "/detail", method = RequestMethod.GET)
+  public String getProductById(@RequestParam Integer id, Model model) {
+
+    Product product = ProductRepository.getProductById(id);
+
+    model.addAttribute("product",product);
+
+    return "detail";
+  }
+
 
 }
